07ac7a6428ae140b223636beab49eb086e3edd75
Sivadej K <bombyk@gmail.com>
2020-04-14 14:25:21 -0800

    add new Company & route

--- models/company.js
+++ models/company.js
@@ -3,27 +3,52 @@
 const db = require('../db');
 const ExpressError = require('../helpers/expressError');
 
-class Company{
+class Company {
+
+	constructor({ handle, name, num_employees, description, logo_url }) {
+		this.handle = handle; 
+		this.name = name; 
+		this.num_employees = num_employees; 
+		this.description = description; 
+		this.logo_url = logo_url;
+	  }
 
 	static async hello() {
-		return([{message:"hello"}, {message:"greetings"}]);
+		return ([{ message: "hello" }, { message: "greetings" }]);
 	}
 
-	static async all() {
-		const results = await db.query(
-			`SELECT handle, name, num_employees, description, logo_url
-				FROM companies`
-		)
+	static async addNew({handle, name, num_employees, description, logo_url}) {
+		const company = await db.query(`
+			INSERT INTO companies (handle, name, num_employees, description, logo_url)
+				VALUES ($1,$2,$3,$4,$5)
+				RETURNING *`,
+			[handle, name, num_employees, description, logo_url]);
+		return company.rows[0];
+	}
+
+	static async all(searchParams) {
+
+		let queryString = `SELECT handle, name, num_employees, description, logo_url FROM companies`;
+		let results;
+
+		// simple get all if no search options received
+		if (Object.keys(searchParams).length === 0) {
+			results = await db.query(queryString);
+		}
+		// add additional params to db query if they exist
+		else {
+			let { search = '', min_employees = 0, max_employees = 2147483647 } = searchParams;
+			if (parseInt(min_employees) > parseInt(max_employees)) throw new ExpressError('Employee min/max search out of range', 404);
+			queryString += ` WHERE name ILIKE $1 AND num_employees BETWEEN $2 AND $3`;
+			results = await db.query(queryString, [`%${search}%`, min_employees, max_employees]);
+		}
+
 		if (!results.rows[0]) {
 			throw new ExpressError('No companies found.', 404);
 		}
-		return results.rows.map(c => ({
-			handle: c.handle,
-			name: c.name,
-			num_employees: c.num_employees,
-			description: c.description,
-			logo_url: c.logo_url,
-		}));
+
+		return results.rows.map(c => new Company(c));
+
 	}
 
 }
--- routes/companies.js
+++ routes/companies.js
@@ -3,17 +3,31 @@ const express = require('express');
 const Company = require('../models/company');
 const router = new express.Router();
 
-router.get('/hello', (req,res,next)=>{
-	res.json({message:'hello from companies'});
+router.get('/hello', (req, res, next) => {
+	res.json({ message: 'hello from companies' });
 })
 
 // GET all companies
 // optional params: search, min_employees, max_employees
 // return JSON { companies: [ companyData, ... ] }
-router.get('/', async (req,res,next)=>{
-	try{
-		let companies = await Company.all();
-		return res.json({companies});
+router.get('/', async (req, res, next) => {
+	try {
+		let companies = await Company.all(req.query);
+		return res.json({ companies });
+	}
+	catch (err) {
+		return next(err);
+	}
+})
+
+// POST /companies
+// Create new and return newly created company data
+// return JSON { company: companyData }
+router.post('/', async (req,res,next) => {
+	try {
+		const { handle, name, num_employees, description, logo_url } = req.body;
+		let company = await Company.addNew({handle, name, num_employees, description, logo_url});
+		return res.json({ company });
 	}
 	catch (err) {
 		return next(err);
